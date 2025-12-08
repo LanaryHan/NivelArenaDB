60f86435c0df260788bb21c3457e8e9278277aea
Lanary <673907311@qq.com>
2025-12-08 16:36:00 +0800

    update card details ui logic

--- Assets/Scripts/Runtime/Business/UI/CardDetail/CardDetailUI.cs
+++ Assets/Scripts/Runtime/Business/UI/CardDetail/CardDetailUI.cs
@@ -1,8 +1,11 @@
+using System.Text;
 using QFramework;
+using Runtime.Business.Data.Entry;
 using Runtime.Business.Manager;
 using Runtime.Business.Util;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Runtime.Business.UI.CardDetail
 {
@@ -22,47 +25,95 @@ namespace Runtime.Business.UI.CardDetail
         public TMP_Text cardIdTxt;
         public TMP_Text cardTypeTxt;
         public TMP_Text cardAttributeTxt;
+        public RectTransform cardTarget;
+        public Button closeBtn;
         [Header("Skill")] 
         public SkillGroup tempSkillGroup;
         public Transform skillContent;
+        [Header("CardInfo")] 
+        public TMP_Text costTxt;
+        public TMP_Text rareTxt;
+        public TMP_Text powerTxt;
+        public TMP_Text hitTxt;
+        public TMP_Text affiliationTxt;
+        public Transform keywordsContent;
+        public TMP_Text tempKeyword;
         
         protected override void OnInit(IUIData uiData = null)
         {
             base.OnInit(uiData);
             tempSkillGroup.gameObject.SetActive(false);
+            tempKeyword.gameObject.SetActive(false);
+            closeBtn.onClick.AddListener(CloseSelf);
         }
 
         protected override void OnOpen(IUIData uiData = null)
         {
             base.OnOpen(uiData);
             skillContent.RemoveAllChildren(tempSkillGroup.transform);
+            keywordsContent.RemoveAllChildren(tempKeyword.transform);
             if (uiData is not CardDetailData cardData)
             {
                 return;
             }
 
+            var ec = GetEventComponent();
             var cardId = cardData.CardId;
             var cardEntry = DataManager.Instance.GetCard(cardId);
             cardNameTxt.text = cardEntry.Name;
             cardIdTxt.text = cardEntry.Id;
             cardTypeTxt.text = cardEntry.CardType.ToChinese();
             cardAttributeTxt.text = cardEntry.Attribute.ToChinese();
+            costTxt.text = cardEntry.Cost == null ? "-" : cardEntry.Cost.Value.ToString();
+            rareTxt.text = cardEntry.Rarity.ToString();
+            powerTxt.text = cardEntry.Power == null ? "-" : cardEntry.Power.Value.ToString();
+            hitTxt.text = cardEntry.Hit == null ? "-" : cardEntry.Hit.Value.ToString();
+            affiliationTxt.text = cardEntry.Affiliation.ToChinese();
             UpdateSkills(cardEntry.Skills);
+            ec.Send(GameEvents.CardFollowReady.Create(cardTarget));
+            ec.Send(GameEvents.ShowCard.Create(cardId));
         }
 
         private void UpdateSkills(int[] skillIds)
         {
-            foreach (var skillId in skillIds)
+            if (skillIds == null || skillIds.Length == 0)
             {
-                var skillGroup = Instantiate(tempSkillGroup, skillContent);
-                skillGroup.Init(skillId);
-                skillGroup.gameObject.SetActive(true);
+                var keywordTxt = Instantiate(tempKeyword, keywordsContent);
+                keywordTxt.text = "-";
+                keywordTxt.gameObject.SetActive(true);
+                return;
             }
+
+            for (var i = 0; i < skillIds.Length; i++)
+            {
+                var skillId = skillIds[i];
+                var skillGroup = Instantiate(tempSkillGroup, skillContent);
+                var skillEntry = DataManager.Instance.GetSkill(skillId);
+                skillGroup.Init(skillEntry);
+                skillGroup.gameObject.SetActive(true);
+
+                var keywordTxt = Instantiate(tempKeyword, keywordsContent);
+                var keyText = GetKeyText(skillEntry);
+                keywordTxt.text = keyText + (i == skillIds.Length - 1 ? "" : ",");
+                keywordTxt.gameObject.SetActive(true);
+            }
+        }
+
+        private string GetKeyText(SkillEntry entry)
+        {
+            var builder = new StringBuilder();
+            builder.Append($"{entry.Key.ToChinese()}");
+            if (!string.IsNullOrEmpty(entry.Param))
+            {
+                builder.Append($":{entry.Param}");
+            }
+            
+            return builder.ToString();
         }
         
         protected override void OnClose()
         {
-            
+            EventManager.Instance.Send(GameEvents.HideCard.Create());
         }
     }
 }
--- Assets/Scripts/Runtime/Business/UI/CardDetail/SkillGroup.cs
+++ Assets/Scripts/Runtime/Business/UI/CardDetail/SkillGroup.cs
@@ -1,8 +1,11 @@
 using System;
+using Coffee.UISoftMask;
 using Runtime.Business.Data;
-using Runtime.Business.Manager;
+using Runtime.Business.Data.Entry;
+using Runtime.Business.Util;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 using Util;
 
@@ -10,35 +13,70 @@ namespace Runtime.Business.UI.CardDetail
 {
     public class SkillGroup : MonoBehaviour
     {
-        public Image skillTypeIcon;
+        public Image skillNormalIcon;
+        public TMP_Text skillNormalTxt;
+        public Image skillLeaderIcon;
+        public TMP_Text skillLeaderTxt;
+        public GameObject specialFront;
+        public SoftMask softMask;
         public TMP_Text skillDescription;
 
-        public void Init(int skillId)
+        public void Init(SkillEntry skillEntry)
         {
-            var skillEntry = DataManager.Instance.GetSkill(skillId);
             skillDescription.text = skillEntry.Description;
             UpdateIcon(skillEntry.Key);
         }
 
         private void UpdateIcon(KeyType key)
         {
-            skillTypeIcon.color = key switch
+            var showNormal = key is not KeyType.Oath and not KeyType.Awakening;
+            skillNormalIcon.gameObject.SetActive(showNormal);
+            skillNormalTxt.gameObject.SetActive(showNormal);
+            skillLeaderIcon.gameObject.SetActive(!showNormal);
+            skillLeaderTxt.gameObject.SetActive(!showNormal);
+            
+            if (showNormal)
             {
-                KeyType.None => Color.white,
-                KeyType.Entry => "#EE7325".ToRGB(),
-                KeyType.Attacker => "#C30D23".ToRGB(),
-                KeyType.Defender => "#028554".ToRGB(),
-                KeyType.Exit => "#555455".ToRGB(),
-                KeyType.Passive => "#601886".ToRGB(),
-                KeyType.Active => "#0D70B8".ToRGB(),
-                KeyType.Guardian => "#0FA0A1".ToRGB(),
-                KeyType.Armed => "#6F3F20".ToRGB(),
-                KeyType.LevelLink => "#5BAB46".ToRGB(),
-                KeyType.FrontConstruction => "#0F5182".ToRGB(),
-                // KeyType.Oath => expr,
-                // KeyType.Awakening => expr,
-                _ => throw new ArgumentOutOfRangeException(nameof(key), key, null)
-            };
+                skillNormalIcon.color = key switch
+                {
+                    KeyType.None => Color.white,
+                    KeyType.Entry => "#EE7325".ToRGB(),
+                    KeyType.Attacker => "#C30D23".ToRGB(),
+                    KeyType.Defender => "#028554".ToRGB(),
+                    KeyType.Exit => "#555455".ToRGB(),
+                    KeyType.Passive => "#601886".ToRGB(),
+                    KeyType.Active => "#0D70B8".ToRGB(),
+                    KeyType.Guardian => "#0FA0A1".ToRGB(),
+                    KeyType.Armed => "#6F3F20".ToRGB(),
+                    KeyType.LevelLink => "#5BAB46".ToRGB(),
+                    KeyType.FrontConstruction => "#0F5182".ToRGB(),
+                    KeyType.Mix => Color.white,
+                    KeyType.Credits => "#C78800".ToRGB(),
+                    KeyType.Escape => "#12A1DE".ToRGB(),
+                    _ => throw new ArgumentOutOfRangeException(nameof(key), key, null)
+                };
+                skillNormalTxt.text = key.ToChinese();
+                if (key is KeyType.Mix)
+                {
+                    softMask.enabled = true;
+                    specialFront.SetActive(true);
+                }
+                else
+                {
+                    softMask.enabled = false;
+                    specialFront.SetActive(false);
+                }
+            }
+            else
+            {
+                (skillLeaderIcon.color, skillLeaderTxt.color) = key switch
+                {
+                    KeyType.Oath => (Color.white, Color.black),
+                    KeyType.Awakening => (Color.black, Color.white),
+                    _ => throw new ArgumentOutOfRangeException(nameof(key), key, null)
+                };
+                skillLeaderTxt.text = key.ToChinese();
+            }
         }
     }
 } 
